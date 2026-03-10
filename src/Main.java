16d5e74f467940c2ccb470b8d8b34cf3581f89dd
Lakshmi98496 <kottadilakshmi897@gmail.com>
2026-03-10 14:24:37 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+/
+public abstract class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    /** Number of beds available in the room. */
+    protected int numberOfBeds;
+
+    /** Total size of the room in square feet. */
+    protected int squareFeet;
+
+    /** Price charged per night for this room type. */
+    protected double pricePerNight;
+
+    /**
+     * Constructor used by child classes to
+     * initialize common room attributes.
+     *
+     * @param numberOfBeds number of beds in the room
+     * @param squareFeet total room size
+     * @param pricePerNight cost per night
+     */
+    public Main(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    /** Displays room details */
+    public void displayRoomDetails() {
+        System.out.println("Room Details:");
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size: " + squareFeet + " sq.ft");
+        System.out.println("Price per Night: ₹" + pricePerNight);
     }
 }
