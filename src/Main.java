abb4daf7bdd3180a11cc513d9d28fc4fb89f8bb0
Lakshmi98496 <kottadilakshmi897@gmail.com>
2026-03-10 14:45:59 +0530

    UC5

--- src/Main.java
+++ src/Main.java
@@ -101,3 +101,30 @@ public class Room {
         }
     }
 }
+public static void main(String[] args) {
+
+    // Display application header
+    System.out.println("Booking Request Queue");
+
+    // Initialize booking queue
+    BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+    // Create booking requests
+    Reservation r1 = new Reservation("Abhi", "Single");
+    Reservation r2 = new Reservation("Subha", "Double");
+    Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+    // Add requests to the queue
+    bookingQueue.addRequest(r1);
+    bookingQueue.addRequest(r2);
+    bookingQueue.addRequest(r3);
+
+    // Display queued booking requests in FIFO order
+    while (bookingQueue.hasPendingRequests()) {
+        Reservation nextRequest = bookingQueue.processNextRequest();
+        System.out.println("Processing Reservation: "
+                + nextRequest.getGuestName()
+                + " - Room Type: "
+                + nextRequest.getRoomType());
+    }
+}
